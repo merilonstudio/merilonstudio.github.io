19de65b650a19963dca4f4cd88072436ecf6707c
Arnaud F <peinorim@users.noreply.github.com>
2017-09-19 10:15:34 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -16,7 +16,7 @@ $(function() {
                 if (currentLocale !== newLocale) {
                     localStorage.setItem("currentLocale", newLocale);
                     localStorage.setItem("currentLocaleDate", Date.now());
-                    location.reload(true);
+                    window.location.reload(true);
                 }
             }
         }
